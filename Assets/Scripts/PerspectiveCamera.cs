e9c1f40e5859de5da487051c7b53ce561107b67c
Neelarghya Mandal <neelarghya.mandal@thoughtworks.com>
2019-11-16 02:42:53 +0530

    [Neel] Add Perspective functionality

--- Assets/Scripts/PerspectiveCamera.cs
+++ Assets/Scripts/PerspectiveCamera.cs
@@ -4,9 +4,21 @@ public class PerspectiveCamera : MonoBehaviour
 {
     [SerializeField] private Transform display;
     private Transform _camera;
+    private Vector3 _initialForward;
 
     private void Start()
     {
         _camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+
+        _initialForward = transform.forward;
+    }
+
+    private void Update()
+    {
+        Vector3 axisOfRotation = Vector3.Cross((display.position - _camera.position), -display.forward).normalized;
+        
+        float angleBetween = Vector3.Angle(_camera.position - display.position, -display.forward);
+        
+        transform.forward = Quaternion.AngleAxis(angleBetween, axisOfRotation) * _initialForward;
     }
 }
