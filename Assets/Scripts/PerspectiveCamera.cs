be23dc117caab5e5515334e7494048638e5647c6
Neelarghya Mandal <neelarghya.mandal@thoughtworks.com>
2019-11-16 01:50:14 +0530

    [Neel] Add Scripts to the cameras

---
+++ Assets/Scripts/PerspectiveCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PerspectiveCamera : MonoBehaviour
+{
+    [SerializeField] private Transform display;
+    private Transform _camera;
+
+    private void Start()
+    {
+        _camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
+}
