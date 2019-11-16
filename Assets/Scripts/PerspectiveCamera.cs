476ed130d00cde0825352398adb4a91e849a7544
Neelarghya Mandal <neelarghya.mandal@thoughtworks.com>
2019-11-17 00:46:59 +0530

    [Neel] Add field of view updating when close to display

--- Assets/Scripts/PerspectiveCamera.cs
+++ Assets/Scripts/PerspectiveCamera.cs
@@ -3,22 +3,39 @@
 public class PerspectiveCamera : MonoBehaviour
 {
     [SerializeField] private Transform display;
+    [SerializeField] private float width = 4f;
     private Transform _camera;
     private Vector3 _initialForward;
+    private Camera _ownCamera;
 
     private void Start()
     {
         _camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
-
+        _ownCamera = GetComponent<Camera>();
         _initialForward = transform.forward;
     }
 
     private void Update()
     {
-        Vector3 axisOfRotation = Vector3.Cross((display.position - _camera.position), -display.forward).normalized;
+        UpdateFieldOfView();
+        UpdateViewDirection();
+    }
+
+    private void UpdateFieldOfView()
+    {
+        float distance = Vector3.Distance(display.position, _camera.position);
+        float fieldOfView = 2 * Mathf.Atan2(width / 2f, distance) * Mathf.Rad2Deg;
         
+        fieldOfView = Mathf.Clamp(fieldOfView, 30, 180);
+        _ownCamera.fieldOfView = fieldOfView;
+    }
+
+    private void UpdateViewDirection()
+    {
+        Vector3 axisOfRotation = Vector3.Cross(display.position - _camera.position, -display.forward).normalized;
+
         float angleBetween = Vector3.Angle(_camera.position - display.position, -display.forward);
-        
+
         transform.forward = Quaternion.AngleAxis(angleBetween, axisOfRotation) * _initialForward;
     }
 }
