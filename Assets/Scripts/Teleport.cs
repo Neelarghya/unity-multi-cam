060b6e7f94aa2f7e38744cf741e9f8e44cff88ce
Neelarghya Mandal <neelarghya.mandal@thoughtworks.com>
2019-11-16 14:00:48 +0530

    [Neel] Retain direction on entering portals

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class Teleport : MonoBehaviour
 {
@@ -8,7 +9,24 @@ public class Teleport : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            other.transform.position = target.position;
+            Transform player = other.transform;
+            Rigidbody rigidbody = player.GetComponent<Rigidbody>();
+            RigidbodyFirstPersonController firstPersonController =
+                player.GetComponent<RigidbodyFirstPersonController>();
+
+            Vector3 axisOfRotation = Vector3.Cross(transform.forward, player.forward).normalized;
+
+            float angleBetween = Vector3.Angle(transform.forward, player.forward);
+
+            player.position = target.position;
+            Vector3 newForward = Quaternion.AngleAxis(angleBetween, axisOfRotation) * target.forward;
+            float angle = Vector3.Angle(player.forward, newForward);
+            int sign = (int) Mathf.Sign(Vector3.Cross(player.forward, newForward).y);
+
+            player.forward = newForward;
+            rigidbody.velocity = player.forward * rigidbody.velocity.magnitude;
+
+            firstPersonController.mouseLook.AddYRotationOffset(sign * angle);
         }
     }
 }
