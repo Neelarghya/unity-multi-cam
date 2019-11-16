1f652babff644c6664dea6bd78f8c70d06a3a687
Neelarghya Mandal <neelarghya.mandal@thoughtworks.com>
2019-11-16 12:55:11 +0530

    [Neel] Add Warping to portals

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = target.position;
+        }
+    }
+}
