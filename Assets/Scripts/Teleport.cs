77e2dee10f66b135d365adf8077eeb458634736d
Neelarghya Mandal <neelarghya.mandal@thoughtworks.com>
2019-11-17 14:41:37 +0530

    [Neel] Retain velocity on entering portal

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -13,20 +13,37 @@ public class Teleport : MonoBehaviour
             Rigidbody rigidbody = player.GetComponent<Rigidbody>();
             RigidbodyFirstPersonController firstPersonController =
                 player.GetComponent<RigidbodyFirstPersonController>();
+            Vector3 transformForward = transform.forward;
 
-            Vector3 axisOfRotation = Vector3.Cross(transform.forward, player.forward).normalized;
-
-            float angleBetween = Vector3.Angle(transform.forward, player.forward);
-
-            player.position = target.position;
-            Vector3 newForward = Quaternion.AngleAxis(angleBetween, axisOfRotation) * target.forward;
-            float angle = Vector3.Angle(player.forward, newForward);
-            int sign = (int) Mathf.Sign(Vector3.Cross(player.forward, newForward).y);
-
-            player.forward = newForward;
-            rigidbody.velocity = player.forward * rigidbody.velocity.magnitude;
-
-            firstPersonController.mouseLook.AddYRotationOffset(sign * angle);
+            SetVelocity(rigidbody, transformForward);
+            SetDirection(player, firstPersonController, transformForward);
         }
+    }
+
+    private void SetDirection(Transform player, RigidbodyFirstPersonController firstPersonController,
+        Vector3 transformForward)
+    {
+        Vector3 axisOfRotation = Vector3.Cross(transformForward, player.forward).normalized;
+        float angleBetween = Vector3.Angle(transformForward, player.forward);
+
+        player.position = target.position;
+        Vector3 newForward = Quaternion.AngleAxis(angleBetween, axisOfRotation) * target.forward;
+        float angle = Vector3.Angle(player.forward, newForward);
+        int sign = (int) Mathf.Sign(Vector3.Cross(player.forward, newForward).y);
+
+        player.forward = newForward;
+
+        firstPersonController.mouseLook.AddYRotationOffset(sign * angle);
+    }
+
+    private void SetVelocity(Rigidbody rigidbody, Vector3 transformForward)
+    {
+        var velocityNormalized = rigidbody.velocity.normalized;
+        Vector3 axisOfRotation = Vector3.Cross(transformForward, velocityNormalized).normalized;
+        float angleBetween = Vector3.Angle(transformForward, velocityNormalized);
+
+        Vector3 newVelocityDirection = Quaternion.AngleAxis(angleBetween, axisOfRotation) * target.forward;
+
+        rigidbody.velocity = newVelocityDirection * rigidbody.velocity.magnitude;
     }
 }
